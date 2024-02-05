2895e3eb3336c619a1cb138f64c898edfb577472
faiznrhlm <157590044+faiznrhlm@users.noreply.github.com>
2024-02-05 07:49:04 +0700

    Update HomePage.jsx

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+
+const MenuList = () => {
+  const menuItems = ['Beef', 'Chiken', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian', 'Breakfast', 'Goat'];
+
+  return (
+      <div>
+          <h2>Menu</h2>
+          <ul>
+              {menuItems.map((item, index) => (
+                  <li key={index}>{item}</li>
+              ))}
+          </ul>
+      </div>
+  );
+};
 
 const HomePage = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
-    </div>
-  )
-}
+      <div>
+          <h1>Welcome to Restaurant GELEGAR</h1>
+          <img src="Gelegar.jpg" alt="Restaurant GELEGAR" />
+          <MenuList />
+      </div>
+  );
+};
+
 
 export default HomePage
